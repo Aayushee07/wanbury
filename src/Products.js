b458a04cb46ea2a420ef1028a9db3c4a3b65cbb3
Bhargav0803 <bhargavmungekar08@gmail.com>
2024-05-21 22:42:14 +0530

    resolved syntax warnings

--- src/Products.js
+++ src/Products.js
@@ -53,16 +53,16 @@ const Products = () => {
         className="w-full h-96"
       />
       <div>
-        <div class="relative z-40 lg:hidden" role="dialog" aria-modal="true">
-          <div class="fixed inset-0 bg-black bg-opacity-25"></div>
+        <div className="relative z-40 lg:hidden" role="dialog" aria-modal="true">
+          <div className="fixed inset-0 bg-black bg-opacity-25"></div>
         </div>
 
-        <main class="max-w-screen px-4 sm:px-6 lg:px-8 py-4">
-          <div class="flex items-baseline justify-between border-b border-gray-200 pb-2 pt-2">
-            <h1 class="text-xl font-bold tracking-tight text-gray-900">
+        <main className="max-w-screen px-4 sm:px-6 lg:px-8 py-4">
+          <div className="flex items-baseline justify-between border-b border-gray-200 pb-2 pt-2">
+            <h1 className="text-xl font-bold tracking-tight text-gray-900">
               Product Catalog
             </h1>
-            <div class="flex items-center">
+            <div className="flex items-center">
            
               {selectedCategories.map((category) => (
                 <span
@@ -91,11 +91,11 @@ const Products = () => {
                 </span>
               ))}
 
-              <div class="relative inline-block text-left">
+              <div className="relative inline-block text-left">
                 <div>
                   <button
                     type="button"
-                    class="group inline-flex justify-center text-sm m-2 font-medium text-gray-700 hover:text-gray-900"
+                    className="group inline-flex justify-center text-sm m-2 font-medium text-gray-700 hover:text-gray-900"
                     id="menu-button"
                     aria-expanded="false"
                     aria-haspopup="true"
@@ -103,21 +103,21 @@ const Products = () => {
                   >
                     Category
                     <svg
-                      class="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+                      className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
                       viewBox="0 0 20 20"
                       fill="currentColor"
                       aria-hidden="true"
                     >
                       <path
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                        clip-rule="evenodd"
+                        clipRule="evenodd"
                       />
                     </svg>
                   </button>
                   <button
                     type="button"
-                    class="group inline-flex justify-center text-sm m-2 font-medium text-gray-700 hover:text-gray-900"
+                    className="group inline-flex justify-center text-sm m-2 font-medium text-gray-700 hover:text-gray-900"
                     id="menu-button"
                     aria-expanded="false"
                     aria-haspopup="true"
@@ -125,15 +125,15 @@ const Products = () => {
                   >
                     Sort
                     <svg
-                      class="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+                      className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
                       viewBox="0 0 20 20"
                       fill="currentColor"
                       aria-hidden="true"
                     >
                       <path
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                        clip-rule="evenodd"
+                        clipRule="evenodd"
                       />
                     </svg>
                   </button>
@@ -237,11 +237,11 @@ const Products = () => {
 
               {/* <button
                 type="button"
-                class="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden"
+                className="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden"
               >
-                <span class="sr-only">Filters</span>
+                <span className="sr-only">Filters</span>
                 <svg
-                  class="h-5 w-5"
+                  className="h-5 w-5"
                   aria-hidden="true"
                   viewBox="0 0 20 20"
                   fill="currentColor"
